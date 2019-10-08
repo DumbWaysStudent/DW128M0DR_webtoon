191c35eea9d5b4260002ce58f814eb28f2b4d848
xavartun <rendiwijiatmoko97@gmail.com>
2019-10-08 22:46:46 +0700

    move file to screens

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Text,View} from 'react-native';
 
-import Login from './src/Login/Login'
+import Login from './src/screens/Login'
 
 export default class App extends Component {
   render(){
