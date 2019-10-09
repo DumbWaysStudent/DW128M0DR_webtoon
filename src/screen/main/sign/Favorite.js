d4e5bfc5aef706cbc357719b165925ad349e6044
xavartun <rendiwijiatmoko97@gmail.com>
2019-10-10 03:34:22 +0700

    my favorite screen

--- src/screen/main/sign/Favorite.js
+++ src/screen/main/sign/Favorite.js
@@ -1,10 +1,86 @@
-import React,{Component} from 'react';
-import {Text} from 'react-native';
+import React, { Component } from 'react';
+import { Container, Content, Button, Text, Icon,Item, Input} from 'native-base';
+import {View, FlatList, TouchableOpacity, Image, StyleSheet,SafeAreaView} from 'react-native'
+import Slideshow from 'react-native-image-slider-show';
 
-export default class FavoriteStack extends Component {
-    render(){
-        return(
-            <Text>FavoriteStack</Text>
-        )
-    }
+
+export default class ForYouStack extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          position: 1,
+          interval: null,
+          banners : [{
+            title: 'The Secret of Angel',
+            url: 'https://cdn.imagecomics.com/assets/i/releases/461826/Mercy_issue1_cvr_147581a7be02116581a0f653533a26b1.jpg',
+            favorite:'110+ favorite'
+          }, {
+            title: 'Pasutri Gaje',
+            url: 'https://akcdn.detik.net.id/community/media/visual/2019/04/03/dac43146-7dd4-49f4-89ca-d81f57b070fc.jpeg?w=770&q=90',
+            favorite:'90 favorite'
+          }, {
+            title: 'Young Mom',
+            url: 'https://cdn.imagecomics.com/assets/i/releases/461620/NomenOmen01-2ndPtg-Cover-2x3_147581a7be02116581a0f653533a26b1.jpg',
+            favorite:'80 favorite'
+          }]
+        };
+      }
+      componentDidMount() {
+        this.setState({
+          interval: setInterval(() => {
+            this.setState({
+              position: this.state.position === this.state.banners.length ? 0 : this.state.position + 1
+            });
+          }, 2000)
+        });
+      }
+
+  render() {
+    const {navigate} = this.props.navigation
+    return (
+      <Container style={{backgroundColor:'#E3E3E3'}}>
+          <Item style={{backgroundColor:'white', borderRadius:15, marginLeft:15, marginRight:15, marginVertical:10}}>
+            <Input placeholder="Search" />
+            <Icon name="ios-search" />
+          </Item>
+        <Content>
+        <SafeAreaView>
+            <FlatList
+                data={this.state.banners}
+                horizontal={false}
+                showsHorizontalScrollIndicator={false}
+                renderItem={({item}) =>
+                    <View style={{backgroundColor:'white',marginHorizontal:15, marginVertical:5, flex:2, flexDirection:'row', borderRadius:15}}>
+                        <View>
+                            <Image style={{width:100, height:100, padding:10, borderRadius:15}} source={{uri : item.url}}/>
+                        </View>
+
+                        <View style={{marginHorizontal:15, alignSelf:'center'}}>
+                            <Text style={{fontSize:18, fontWeight:'bold', marginBottom:10}}>{item.title}</Text>
+                            <View>
+                                <Text style={{color:'#676767', marginBottom:10}}> {item.favorite}</Text>
+                            </View>
+                        </View>
+                    </View> 
+                }                
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </SafeAreaView>
+        
+        </Content>
+      </Container>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+    title:{
+        marginHorizontal:15,
+        marginVertical:10,
+        fontSize:25,
+        fontWeight:"bold"
+    },
+    btnActive:{
+        color:"#00D163"
+    }
+})
