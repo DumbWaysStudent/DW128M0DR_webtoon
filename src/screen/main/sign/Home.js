cf7a374c23566ca7b1a224768c1ac1a6ef8f9f15
xavartun <rendiwijiatmoko97@gmail.com>
2019-10-10 01:02:37 +0700

    update style

--- src/screen/main/sign/Home.js
+++ src/screen/main/sign/Home.js
@@ -33,7 +33,7 @@ export default class ForYouStack extends Component {
       }
 
   render() {
-    const navigate = this.props.navigation.navigate('DetailStack')
+    const {navigate} = this.props.navigation
     return (
       <Container style={{backgroundColor:'#E3E3E3'}}>
           <Item style={{backgroundColor:'white', borderRadius:15, marginLeft:15, marginRight:15, marginVertical:10}}>
@@ -63,7 +63,7 @@ export default class ForYouStack extends Component {
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
                 renderItem={({item}) =>
-                <TouchableOpacity onPress={()=>console.log(navigate)}>
+                <TouchableOpacity onPress={()=>navigate('DetailStack', {url:item.url, title:item.title})}>
                     <View style={{marginHorizontal:10, backgroundColor:'white', borderRadius:10}}>
                         <Image style={{width:'100%', height:150, padding:10, borderTopLeftRadius:10, borderTopRightRadius:10}} source={{uri : item.url}}/>
                         <View style={{width : 150}}>
